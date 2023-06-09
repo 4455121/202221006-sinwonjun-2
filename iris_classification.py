f52d11e3d5711a1104756820b54b2c428713161c
4455121 <134983471+4455121@users.noreply.github.com>
2023-06-10 00:56:43 +0900

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -3,7 +3,6 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-import numpy as np
 
 def iris_classification():
     # Iris 데이터셋 로드
@@ -28,21 +27,5 @@ def iris_classification():
     st.subheader("Iris Classification")
     st.write("Accuracy:", accuracy)
 
-def initialize_centroids(data, k):
-    centroids = data[np.random.choice(data.shape[0], k, replace=False)]
-    return centroids
-
 if __name__ == '__main__':
-    # 예시 데이터 생성
-    data = np.random.randn(100, 2)
-
-    # k-means 클러스터링을 위한 클러스터 개수 입력
-    k = 3
-
-    # 클러스터 중심 초기화
-    centroids = initialize_centroids(data, k)
-
-    # k-means 클러스터링 결과 출력
-    st.subheader("K-means Clustering")
-    st.write("Cluster Centers:")
-    st.write(centroids)
+    iris_classification()
